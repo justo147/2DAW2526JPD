2e8fdcaaefc9dfeae1eeb1c88d6847e737a6a97d
DESKTOP-HHGOI4D\Justo <justo147@hotmail.com>
2025-09-26 12:09:17 +0200

    Actividad propuesta 2.5

---
+++ Tema 2/ActividadPropuesta2.5.js
@@ -0,0 +1,10 @@
+let nombre=prompt("Escribe tu nombre")
+let salida=confirm("Deseas salir del programa?")
+if(salida){
+    alert("has decidido salir del programa")
+    window.close()
+    console.log("<b>Saliendo del programa</b>")
+}else{
+    alert("Has decidido quedarte")
+}
+
